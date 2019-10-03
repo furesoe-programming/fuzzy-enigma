2419da7ef0ddc0053be7d1f89e88affdd72de960
Something <furesoe.programming@gmail.com>
2019-10-03 17:59:33 +0200

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+		Rigidbody m_rigid;
+		public float power = 50f;
+
+	// Use this for initialization
+	 void Start () {
+		 m_rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position += transform.forward * 1.0f;
+
+		m_rigid.AddForce(transform.forward * power);
+	
+
+	}
+}
